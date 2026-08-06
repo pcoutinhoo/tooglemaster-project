1cae46c32461c399b6b8e67305e12ffc1a206ee8
Rodrigo <rodrigoalbuquerque07@outlook.com>
2026-08-05 21:52:40 -0300

    feat: :sparkles: correcao das versoes
    correcao das versoes

---
+++ infra/app/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket       = "togglemaster-tfstate-445903040232"
+    key          = "togglemaster/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
+}
